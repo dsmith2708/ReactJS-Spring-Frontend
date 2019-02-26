f25bbfb96ff293415c5adf08b1e476a266cce94c
Daniel Smith <danielsmith.2796@googlemail.com>
2019-02-26 11:11:56 +0000

    html elements added for create user form

--- src/components/create/Create.js
+++ src/components/create/Create.js
@@ -3,7 +3,20 @@ import React from 'react';
 export class Create extends React.Component {
     render() {
         return (
-            <h1>Create works!</h1>
+            <div>
+                <h1>Create works!</h1>
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="nameInput">Name:</label>
+                        <input type="text" className="form-control" id="nameInput" placeholder="Enter name" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="ageInput">Age:</label>
+                        <input type="number" className="form-control" id="ageInput" placeholder="Enter Age" />
+                    </div>
+                    <button className="btn btn-primary">Add User</button>
+                </form>
+            </div>
         )
     }
 }
